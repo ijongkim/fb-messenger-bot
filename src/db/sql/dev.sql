2ff1a728ce957302e6cfc9b36340371a7bd08004
Keith Alpichi <keithalpichi@gmail.com>
2017-06-25 21:03:49 -0700

    edited SQL scripts to insert seed data

--- src/db/sql/dev.sql
+++ src/db/sql/dev.sql
@@ -10,3 +10,8 @@ CREATE TABLE IF NOT EXISTS addresses (
   full_address text NOT NULL,
   user_id INTEGER NOT NULL REFERENCES users ON DELETE CASCADE
 );
+
+INSERT INTO users (username) VALUES ('admin');
+INSERT INTO users (username) VALUES ('guest');
+INSERT INTO addresses (full_address, user_id) VALUES ('1 Market St, San Francisco CA 94105', 1);
+INSERT INTO addresses (full_address, user_id) VALUES ('1 Telegraph Hill Blvd, San Francisco, CA 94133', 2);
