f1a6963b1e4a9716b9ce51d02b86838d3673b983
Keith Alpichi <keithalpichi@gmail.com>
2017-06-27 13:13:40 -0700

    edited query to return description of task

--- src/db/task.js
+++ src/db/task.js
@@ -1,9 +1,11 @@
 const db = require('./index')
 
-module.exports.selectTasks = ({ user_id }) => db.manyOrNone(`SELECT * FROM tasks where user_id = '${user_id}'`)
+module.exports.selectTasks = ({ user_id }) => db.manyOrNone(`SELECT * FROM tasks WHERE user_id = '${user_id}'`)
 
-module.exports.selectTaskById = ({ id }) => db.oneOrNone(`SELECT * FROM tasks where id = ${id}`)
+module.exports.selectTaskById = ({ id }) => db.oneOrNone(`SELECT * FROM tasks WHERE id = ${id}`)
 
 module.exports.insertTask = ({ description, user_id }) => db.none(`INSERT INTO tasks (description, user_id) VALUES ('${description}', '${user_id}')`)
 
-module.exports.markAsComplete = ({ id }) => db.none(`UPDATE tasks SET completed = true WHERE id = ${id}`)
+module.exports.markAsComplete = ({ id }) => db.none(`UPDATE tasks SET completed = true WHERE id = ${id} RETURNING description`)
+
+module.exports.selectCompletedTasks = ({ user_id }) => db.manyOrNone(`SELECT * FROM tasks WHERE user_id = '${user_id} AND completed = true'`)
