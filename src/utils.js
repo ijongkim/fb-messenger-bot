7546c85ddc0a90b8c442ab3962b999ddb10e6a97
Keith Alpichi <keithalpichi@gmail.com>
2017-06-27 13:13:26 -0700

    added util functions

---
+++ src/utils.js
@@ -0,0 +1,53 @@
+const axios = require('axios')
+const User = require('./db/User')
+const REQUEST_URL = `https://graph.facebook.com/v2.6/me/messages`
+const HELP_MSG = [
+  'Type the following commands: ',
+  'HELP: for these instructions',
+  'LIST: for a list of all your tasks',
+  `#<number> DONE: to mark a task as complete. For example, '#4 DONE' will mark task #4 as complete`,
+  `ADD <task description>: to add a new task with the description. For example, 'ADD buy milk' will add this as a new task`,
+  'LIST DONE: for a list of all completed tasks'
+].join('\n')
+
+const constructResponse = ({ senderID, text }) => ({ recipient: { id: senderID }, message: { text: text } })
+
+const sendRequest = data => {
+  return axios({
+    url: REQUEST_URL,
+    params: {
+      access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN
+    },
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    data: data
+  })
+  .then(resp => {
+    console.log(`Response data from request: `, resp.data)
+  })
+  .catch(err => {
+    console.warn(`Error sending request: `, err.message)
+  })
+}
+
+module.exports.verifyAuth = senderID => {
+  return User.selectUserById({ user_id: senderID })
+  .then(user => {
+    if (user) {
+      return Promise.resolve()
+    } else {
+      throw new Error('User not found in DB')
+    }
+  })
+  .catch(_ => {
+    return User.insertUser({ id: senderID })
+    .then(() => {
+      const data = constructResponse({senderID: senderID, text: HELP_MSG})
+      return sendRequest(data)
+    })
+  })
+}
+
+module.exports.HELP_MSG
+module.exports.sendRequest
+module.exports.constructResponse
