1108f9333e0daa8be5f53538ac923df78c1c9cb4
Keith Alpichi <keithalpichi@gmail.com>
2017-06-27 13:45:36 -0700

    edit User query for insert to do nothing on dups

--- src/db/user.js
+++ src/db/user.js
@@ -1,5 +1,5 @@
 const db = require('./index')
 
-module.exports.insertUser = ({ id }) => db.none(`INSERT INTO users (id) VALUES ('${id}')`)
+module.exports.insertUser = ({ id }) => db.none(`INSERT INTO users (id) VALUES ('${id}') ON CONFLICT DO NOTHING`)
 
 module.exports.selectUserById = ({ id }) => db.oneOrNone(`SELECT * from users where id = '${id}'`)
